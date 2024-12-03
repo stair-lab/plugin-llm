fa90387f2da6aca2ff0a10c10a92ab0f94c915bd
haolun-wu <haolun.wu@mail.mcgill.ca>
2024-12-02 23:49:25 -0500

    update setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
+# Ensure the script exits immediately if a command fails
+set -e
 
-cd
-sudo apt install -y python3.11
-sudo apt install -y python3.11-venv
-python3.11 -m venv statml
-source statml/bin/activate
+# Assuming the environment is already activated, start directly with package installation
+echo "Installing Python packages..."
 
 pip install torch==2.4
 pip install pyyaml transformers pandas datasets scikit-learn tensorboardX
 pip install 'accelerate>=0.26.0'
 
-pip install evaluate 
-pip install  nlg-metricverse
+pip install evaluate
+pip install nlg-metricverse
 
 pip install --upgrade datasets
 
@@ -19,6 +18,26 @@ pip install numpy==1.26.0 pycocoevalcap
 
 pip install nltk rouge_score
 
-cd
-deactivate
+echo "Installation complete!"
+# cd
+# sudo apt install -y python3.11
+# sudo apt install -y python3.11-venv
+# python3.11 -m venv statml
+# source statml/bin/activate
+
+# pip install torch==2.4
+# pip install pyyaml transformers pandas datasets scikit-learn tensorboardX
+# pip install 'accelerate>=0.26.0'
+
+# pip install evaluate 
+# pip install  nlg-metricverse
+
+# pip install --upgrade datasets
+
+# pip install numpy==1.26.0 pycocoevalcap
+
+# pip install nltk rouge_score
+
+# cd
+# deactivate
 
