7ebd89b40f891888012bc5af27a65aad01f983df
haolun-wu <haolun.wu@mail.mcgill.ca>
2024-12-02 23:44:43 -0500

    update

---
+++ bin/setup-mila-server.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Ensure the script exits immediately if a command fails
+set -e
+
+# Assuming the environment is already activated, start directly with package installation
+echo "Installing Python packages..."
+
+pip install torch==2.4
+pip install pyyaml transformers pandas datasets scikit-learn tensorboardX
+pip install 'accelerate>=0.26.0'
+
+pip install evaluate
+pip install nlg-metricverse
+
+pip install --upgrade datasets
+
+pip install numpy==1.26.0 pycocoevalcap
+
+pip install nltk rouge_score
+
+echo "Installation complete!"
